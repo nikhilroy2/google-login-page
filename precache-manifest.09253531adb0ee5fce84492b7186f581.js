fe193af5a0e40051d93e12e84a2959cf185526b0
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2020-03-13 19:04:34 +0600

    Updates

---
+++ precache-manifest.09253531adb0ee5fce84492b7186f581.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce7aad0edce88bbd29749cd8b5e301f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2531160f0346dd6b6dd1",
+    "url": "./static/css/main.b67bce67.chunk.css"
+  },
+  {
+    "revision": "018ebf55c205f25e2ed0",
+    "url": "./static/js/2.14d5332f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.14d5332f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2531160f0346dd6b6dd1",
+    "url": "./static/js/main.8c6c41b7.chunk.js"
+  },
+  {
+    "revision": "1d1feb9ac959fa64e717",
+    "url": "./static/js/runtime-main.33d7aa7f.js"
+  }
+]);
